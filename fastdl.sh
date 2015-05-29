d39c7302dec15b66822f1e4d18752838676a0d0a
Wenjun.Zhang <zhangwenjun1@meizu.com>
2015-05-29 18:45:31 +0800

    Add fastboot tool.

---
+++ fastdl.sh
@@ -0,0 +1,17 @@
+OUTDIR=$1
+
+CMD='fastboot flash'
+
+#$CMD lk lk.bin
+$CMD boot $OUTDIR/boot.img
+$CMD recovery $OUTDIR/recovery.img
+$CMD secro $OUTDIR/secro.img
+$CMD logo $OUTDIR/logo.bin
+$CMD custom $OUTDIR/custom.img
+$CMD tee1 $OUTDIR/trustzone.bin
+$CMD tee2 $OUTDIR/trustzone.bin
+$CMD system $OUTDIR/system.img
+$CMD cache $OUTDIR/cache.img
+$CMD userdata $OUTDIR/userdata.img
+
+fastboot reboot 
